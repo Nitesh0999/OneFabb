e6ce1cdea6026ba8984e5a95d6ceda0883f6e0a1
Nitesh Verma <43405482+Nitesh0999@users.noreply.github.com>
2021-11-04 09:10:22 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -7,11 +7,11 @@ $message= $_POST['message'];
 $to = "nitesh.verma0999@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com" . "\r\n" .
-"CC: somebodyelse@example.com";
+$headers = "From: nitesh.gkv@gmail.com" . "\r\n" .
+"CC: nitesh_verma@s3india.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
 //redirect
-header("Location:thankyou.html");
+header("Location:services.html");
 ?>
