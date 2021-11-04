3af5a26858a027318e03cb3b16dcb8ea26dd179c
Nitesh Verma <43405482+Nitesh0999@users.noreply.github.com>
2021-11-04 11:49:35 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,17 +1,5 @@
 <?php
 //get data from form  
 
-$name = $_POST['name'];
-$email= $_POST['email'];
-$message= $_POST['message'];
-$to = "nitesh.verma0999@gmail.com";
-$subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: nitesh.gkv@gmail.com" . "\r\n" .
-"CC: nitesh_verma@s3india.com";
-if($email=NULL){
-    mail($to,$subject,$txt,$headers);
-}
-//redirect
-header("Location:services.html");
+echo "button Got Clicked";
 ?>
